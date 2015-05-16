7d7172b33067b5a2e78286bc59d8974bf48a475d
LeeYelim <pkmarin15@naver.com>
2015-05-16 14:46:13 +0900

    0516 14:45 xml 수정

--- src/com/banana/banana/signup/SymtomItemData.java
+++
@@ -1,8 +0,0 @@
-package com.banana.banana.signup;
-
-
-public class SymtomItemData{
-public String syndrome_name;
-public String syndrome_before;
-public String syndrome_after;  
-}
--- src/com/banana/banana/signup/SymtomListItemData.java
+++
@@ -1,7 +0,0 @@
-package com.banana.banana.signup;
-
-public class SymtomListItemData {
-	public String symtom;
-	public int days_before;
-	public int days_after;
-}
